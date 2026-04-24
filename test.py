1b74f0afbc7559017df91d0cd1b7a10b928e8c51
22059-sudo <22059@burnside.school.nz>
2026-04-24 12:25:35 +1200

    first commit

---
+++ test.py
@@ -0,0 +1,33 @@
+from flask import Flask, g
+import sqlite3
+
+DATABASE = 'database.db'
+
+
+app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def home():
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM Tech;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    return "Welcome to tech hub"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+     
