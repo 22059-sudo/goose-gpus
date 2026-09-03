a6d09aae37b5a2bde397d28a4c969566ffc9a4e5
22059-sudo <22059@burnside.school.nz>
2026-09-04 11:16:56 +1200

    commit 6

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
-from flask import Flask, g, render_template
 import sqlite3
+from flask import Flask, g, render_template
 
 DATABASE = 'database.db'
-
-
 app = Flask(__name__)
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        # Allows you to use column names like gpu['name'] in your HTML template
+        db.row_factory = sqlite3.Row
     return db
 
 @app.teardown_appcontext
@@ -18,19 +18,18 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-    
-
 @app.route('/')
 def home():
     db = get_db()
     cursor = db.cursor()
+    
+    # Query your table
     sql = "SELECT * FROM gpus;"
     cursor.execute(sql)
     results = cursor.fetchall()
-    return str(results)
-
-    return render_template("home.html",results=results)
+    
+    return render_template("home.html", results=results)
 
 if __name__ == "__main__":
     app.run(debug=True)
-     
+
